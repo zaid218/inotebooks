5ba37ddbf56db4e4ba081de6f03ab26fd5ead0f2
Mohd ZAID KHAN <zaidk9620@gmail.com>
2023-03-09 17:09:23 +0530

    ab changes kr diya ,ye theek hai

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://127.0.0.1:27017/test?directConnection=true"
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log('connected to Mongo successfully')
-    },6000)
+    })
 }
 module.exports=connectToMongo
