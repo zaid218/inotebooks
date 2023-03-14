a274e0a34a1989fc72adeb961d6f32bb10f6b2d7
Mohd ZAID KHAN <zaidk9620@gmail.com>
2023-03-15 00:03:40 +0530

    video 52 using bcrypt

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true"
+const mongoURI = "mongodb://127.0.0.1:27017/named?directConnection=true"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log('connected to Mongo successfully')
     })
 }
-module.exports=connectToMongo
+module.exports = connectToMongo
--- backend/index.js
+++ backend/index.js
@@ -14,3 +14,4 @@ app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
